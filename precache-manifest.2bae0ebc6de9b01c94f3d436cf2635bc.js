e80431f3f8630817279fdacd8d3a1889a47b316d
GaryGosh <clichethe1@yahoo.com>
2020-07-18 21:01:25 +0530

    Updates

---
+++ precache-manifest.2bae0ebc6de9b01c94f3d436cf2635bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51888cb390f0242abb7d310904654f33",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "a90c6594a230f381ba1f",
+    "url": "/Calculator/static/css/main.b299b252.chunk.css"
+  },
+  {
+    "revision": "96e87d43e9514494a063",
+    "url": "/Calculator/static/js/2.5df6ed0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.5df6ed0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90c6594a230f381ba1f",
+    "url": "/Calculator/static/js/main.8da7b098.chunk.js"
+  },
+  {
+    "revision": "36cda1db55e6e62de084",
+    "url": "/Calculator/static/js/runtime-main.c4967e62.js"
+  }
+]);
